cdcfd6fe557b97a7872c848f4a3329249c8d1acb
Vinícius Fernando <vinicius.fd.ferreira835@gmail.com>
2026-05-25 21:55:32 -0300

    Add Pizza class with attributes and methods

--- PizzariaEntrega.java
+++ PizzariaEntrega.java
@@ -1,1 +1,56 @@
+//Viniícius 
+public class Pizza {
 
+    private String sabor;
+
+    private String tamanho;
+
+    private double precoBase;
+
+
+
+    public Pizza(String sabor, String tamanho, double precoBase) {
+
+        this.sabor = sabor;
+
+        this.tamanho = tamanho;
+
+        this.precoBase = precoBase;
+
+    }
+
+
+
+    public String getSabor() { return sabor; }
+
+    public void setSabor(String sabor) { this.sabor = sabor; }
+
+
+
+    public String getTamanho() { return tamanho; }
+
+    public void setTamanho(String tamanho) { this.tamanho = tamanho; }
+
+
+
+    public double getPrecoBase() { return precoBase; }
+
+    public void setPrecoBase(double precoBase) { this.precoBase = precoBase; }
+
+
+
+    public double calcularPrecoFinal() {
+
+        return precoBase;
+
+    }
+
+
+
+    public void exibirDetalhesPizza() {
+
+        System.out.println("- 1x Pizza " + this.tamanho + " de " + this.sabor + " (R$ " + this.precoBase + ")");
+
+    }
+
+}
