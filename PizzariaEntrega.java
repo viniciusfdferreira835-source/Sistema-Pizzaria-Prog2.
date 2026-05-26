a87f5241ec8e428c5d102779d5bf045357274ac6
Vinícius Fernando <vinicius.fd.ferreira835@gmail.com>
2026-05-25 21:56:16 -0300

    Add Cliente class with attributes and methods

--- PizzariaEntrega.java
+++ PizzariaEntrega.java
@@ -54,3 +54,52 @@ public class Pizza {
     }
 
 }
+public class Cliente {
+
+    private String nome;
+
+    private String telefone;
+
+    private String endereco;
+
+
+
+    public Cliente(String nome, String telefone, String endereco) {
+
+        this.nome = nome;
+
+        this.telefone = telefone;
+
+        this.endereco = endereco;
+
+    }
+
+
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+
+
+    public String getTelefone() { return telefone; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+
+
+    public String getEndereco() { return endereco; }
+
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+
+
+    public void exibirInfoCliente() {
+
+        System.out.println("Cliente: " + this.nome + " | Tel: " + this.telefone);
+
+        System.out.println("Endereço: " + this.endereco);
+
+    }
+
+}
